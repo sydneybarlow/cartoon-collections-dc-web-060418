e7e7e3933cb2815faa55f9787f11325c91f8926a
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 13:35:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,12 +18,7 @@ def long_planeteer_calls(calls)
   end
 end
 
-# words.any? do |word|
-#   word.length > 4
-# end
-
-
-# def find_the_cheese# code an argument here
-#   # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-# end
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
