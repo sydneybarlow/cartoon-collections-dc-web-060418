1775514a389a2bba7a788d3b3112a192674786c6
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 13:24:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  counter = 1 
+  names.each_with_index do |dwarves|
+    puts "#{counter}. #{dwarves}"
+    counter += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |calls|
+    calls.capitalize + !
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
