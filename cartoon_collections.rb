8aced95510d27b3a82045a154e7d5a0668186495
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 13:39:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,5 @@ end
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
   ingredients = cheese_types
-  ingredients.include?(0)
+  ingredients.include?(cheddar)
 end
